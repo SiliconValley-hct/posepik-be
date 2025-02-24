7f28876e40a7195cd23fc14b5124e0f169a5247e
SHIN__0.0 <ajtwoddltom@gmail.com>
2025-02-24 10:36:48 -0800

    setting: basic

---
+++ src/main/java/hackthevalley/posepik_be/PosepikBeApplication.java
@@ -0,0 +1,13 @@
+package hackthevalley.posepik_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PosepikBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PosepikBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hackthevalley/posepik_be/PosepikBeApplicationTests.java
@@ -0,0 +1,13 @@
+package hackthevalley.posepik_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PosepikBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
